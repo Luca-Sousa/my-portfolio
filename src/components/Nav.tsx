7fcfeaab5aab3da4e63b5ced312770eaf8f7b639
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-07-16 11:30:43 -0300

    Update

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,14 +10,14 @@ export function Nav() {
   };
 
   return (
-    <div className="max-w-[1300px] mx-auto bg-zinc-900 font-PrimaryFont font-medium uppercase shadow-shape xl:max-w-full xl:mx-4 lg:mx-8">
+    <div className="max-w-[1300px] mx-auto bg-zinc-900 font-PrimaryFont font-medium uppercase shadow-shape xl:max-w-full xl:mx-4 lg:mx-8 md:mx-0">
       <nav className="h-20 flex justify-between items-center sm:flex-col sm:h-36 sm:py-2">
         <div className="flex items-center gap-8">
           <img className="size-20 sm:rounded-full sm:size-14" src={imgLogo} alt="Image Logo" />
           <h2 className="text-zinc-100 tracking-widest text-2xl">Lucas</h2>
         </div>
 
-        <ul className="h-full flex items-center gap-12 lg:mr-4 lg:gap-6 sm:mr-0">
+        <ul className="h-full flex items-center gap-12 lg:gap-6 md:mr-8 sm:mr-0">
           <li
             onClick={() => handleClick("home")}
             className={`${
