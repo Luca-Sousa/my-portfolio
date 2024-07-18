7c7a19d7a7e402484cbc1bb5c20badc9a89af6b5
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-07-18 12:36:07 -0300

    Update SendEmail.ts

--- src/api/SendEmail.ts
+++ src/api/SendEmail.ts
@@ -1,35 +1,35 @@
 // api/sendEmail.ts
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import nodemailer from 'nodemailer';
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 587,
   auth: {
-    user: process.env.EMAIL_USER, // Variável de ambiente para o e-mail
-    pass: process.env.EMAIL_PASSWORD, // Variável de ambiente para a senha
+    user: process.env.EMAIL_USER!, // Variável de ambiente para o e-mail
+    pass: process.env.EMAIL_PASSWORD!, // Variável de ambiente para a senha
   },
 });
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' });
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
   }
 
   const { name, email, message } = req.body;
 
   const mailOptions = {
     from: email,
-    to: process.env.EMAIL_TO, // Variável de ambiente para o e-mail de recebimento
-    subject: `New message from ${name}`,
+    to: process.env.EMAIL_TO!, // Variável de ambiente para o e-mail de recebimento
+    subject: `Nova mensagem de ${name}`,
     text: message,
   };
 
   try {
     await transporter.sendMail(mailOptions);
-    return res.status(200).json({ message: 'Email sent successfully' });
+    return res.status(200).json({ message: "E-mail enviado com sucesso" });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'Failed to send email' });
+    return res.status(500).json({ message: "Falha ao enviar e-mail" });
   }
 };
